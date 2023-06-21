faea70eaef41ae5277104477b533177bb70c141d
rradjab <rradjab@yahoo.com>
2023-06-21 17:12:30 +0400

    ra

--- horizontal_list/lib/main.dart
+++ horizontal_list/lib/main.dart
@@ -30,6 +30,14 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
+  int currentIndex = 0;
+  final ScrollController scrollController = ScrollController();
+  @override
+  void dispose() {
+    scrollController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -40,30 +48,37 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
         child: LayoutBuilder(builder: (context, constraints) {
           double width = constraints.constrainWidth();
 
-          print(width);
-          return width < 500
-              ? GridView.builder(
-                  key: const PageStorageKey<String>('page'),
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2),
-                  itemCount: 300,
-                  itemBuilder: (context, index) => Card(
-                    elevation: 15,
-                    child: Center(child: Text('Item $index')),
+          debugPrint(width.toString());
+          currentIndex = width < 500 ? 0 : 1;
+          return IndexedStack(
+            index: currentIndex,
+            children: [
+              GridView.builder(
+                //key: const PageStorageKey<String>('page'),
+                controller: scrollController,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2),
+                itemCount: 300,
+                itemBuilder: (context, index) => Card(
+                  elevation: 15,
+                  child: Center(child: Text('Item $index')),
+                ),
+              ),
+              ListView.builder(
+                //key: const PageStorageKey<String>('page'),
+                controller: scrollController,
+                itemCount: 300,
+                itemBuilder: (context, index) => ListTile(
+                  title: Column(
+                    children: [
+                      Center(child: Text('Item $index')),
+                      const Divider()
+                    ],
                   ),
-                )
-              : ListView.builder(
-                  key: const PageStorageKey<String>('page'),
-                  itemCount: 300,
-                  itemBuilder: (context, index) => ListTile(
-                    title: Column(
-                      children: [
-                        Center(child: Text('Item $index')),
-                        const Divider()
-                      ],
-                    ),
-                  ),
-                );
+                ),
+              )
+            ],
+          );
         }),
       ),
     );
